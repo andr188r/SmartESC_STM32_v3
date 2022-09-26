d972e7cc1af195a5ac205aedf10e969643e14a3d
andr188r <114382739+andr188r@users.noreply.github.com>
2022-09-26 19:42:00 +0200

    Update config.h
    Gg4

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -18,7 +18,7 @@
 #define REGEN_MAX_CURRENT 10000
 
 // maximum current for field weakening in mA
-#define FIELD_WEAKNING_CURRENT_MAX 30000 //max id
+#define FIELD_WEAKNING_CURRENT_MAX 3000 //max id
 
 // ADC channels
 #define ADC_VOLTAGE 0
